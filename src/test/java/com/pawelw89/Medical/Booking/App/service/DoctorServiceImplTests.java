29eeec9c7f2d32698d46f6116516ec88b27ef0c9
pawelw89 <pawelwojciechowski89@wp.pl>
2021-05-14 20:42:18 +0200

    Added some testes to DoctorServiceImplTests

--- src/test/java/com/pawelw89/Medical/Booking/App/service/DoctorServiceImplTests.java
+++ src/test/java/com/pawelw89/Medical/Booking/App/service/DoctorServiceImplTests.java
@@ -1,7 +1,6 @@
 package com.pawelw89.Medical.Booking.App.service;
 
 import entity.Doctor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +8,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import repository.DoctorRepository;
 import service.DoctorServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class DoctorServiceImplTests {
@@ -20,17 +24,31 @@ public class DoctorServiceImplTests {
     @InjectMocks
     private DoctorServiceImpl doctorServiceImpl;
 
+    @Test
+    public void  shouldReturnAllDoctors(){
+        List<Doctor> doctors = new ArrayList<>();
+        Doctor doctor1 = new Doctor();
+        Doctor doctor2 = new Doctor();
+        Doctor doctor3 = new Doctor();
+        doctors.add(doctor1);
+        doctors.add(doctor2);
+        doctors.add(doctor3);
+
+        List <Doctor> savedDoctors = doctorRepository.saveAll(doctors);
+        List <Doctor> foundDoctors = doctorServiceImpl.getAllDoctors();
+
+        assertEquals(savedDoctors, foundDoctors);
+    }
 
     @Test
     public void shouldReturnDoctorByPesel(){
         long pesel = 1234567890;
         Doctor doctor = new Doctor();
         doctor.setPesel(pesel);
-        Doctor savedDoctor = doctorRepository.saveAndFlush(doctor);
+        Doctor savedDoctor = doctorRepository.save(doctor);
+        Doctor foundDoctor = doctorServiceImpl.findDoctorByPesel(pesel);
 
-        Doctor foundDoctor = doctorRepository.getOne(pesel);
-
-        Assertions.assertEquals(savedDoctor, foundDoctor);
+        assertEquals(savedDoctor, foundDoctor);
     }
 
 }
