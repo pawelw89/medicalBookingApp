fbcfedb2abaf9f7eca8fce26a195f1f906d3e746
pawelw89 <pawelwojciechowski89@wp.pl>
2021-05-18 16:00:14 +0200

    Added some noe testes to DoctorServiceImplTests - shouldAddDoctor - shouldDeleteDoctor

--- src/test/java/com/pawelw89/Medical/Booking/App/service/DoctorServiceImplTests.java
+++ src/test/java/com/pawelw89/Medical/Booking/App/service/DoctorServiceImplTests.java
@@ -1,6 +1,8 @@
 package com.pawelw89.Medical.Booking.App.service;
 
 import entity.Doctor;
+import entity.Speciality;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,14 +11,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import repository.DoctorRepository;
 import service.DoctorServiceImpl;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DoctorServiceImplTests {
-
 
     @Mock
     private DoctorRepository doctorRepository;
@@ -24,31 +31,67 @@ public class DoctorServiceImplTests {
     @InjectMocks
     private DoctorServiceImpl doctorServiceImpl;
 
+    Doctor d1 = new Doctor();
+    long pesel = 12345678901l;
+    Doctor d2 = new Doctor();
+    Doctor d3 = new Doctor();
+
+
     @Test
-    public void  shouldReturnAllDoctors(){
-        List<Doctor> doctors = new ArrayList<>();
-        Doctor doctor1 = new Doctor();
-        Doctor doctor2 = new Doctor();
-        Doctor doctor3 = new Doctor();
-        doctors.add(doctor1);
-        doctors.add(doctor2);
-        doctors.add(doctor3);
+    public void shouldReturnAllDoctors(){
 
-        List <Doctor> savedDoctors = doctorRepository.saveAll(doctors);
-        List <Doctor> foundDoctors = doctorServiceImpl.getAllDoctors();
+        given(doctorRepository.findAll()).willReturn(Arrays.asList(d1, d2, d3));
 
-        assertEquals(savedDoctors, foundDoctors);
+        List<Doctor> result = doctorServiceImpl.getAllDoctors();
+
+        assertThat(result).hasSize(3);
     }
 
     @Test
     public void shouldReturnDoctorByPesel(){
-        long pesel = 1234567890;
-        Doctor doctor = new Doctor();
-        doctor.setPesel(pesel);
-        Doctor savedDoctor = doctorRepository.save(doctor);
+        d1.setPesel(pesel);
+        given(doctorRepository.getOne(pesel)).willReturn(d1);
+
         Doctor foundDoctor = doctorServiceImpl.findDoctorByPesel(pesel);
 
-        assertEquals(savedDoctor, foundDoctor);
+        assertEquals(d1, foundDoctor);
+    }
+
+    //trzeba nad tym jeszcze popracować
+    @Test
+    public void shouldUpdateDoctor(){
+        d1.setPesel(pesel);
+        d2.setFirstName("Jan");
+        d2.setSurname("Nowak");
+        d2.setFirstName("Piotr");
+        d2.setSurname("Żyła");
+        given(doctorRepository.getOne(pesel)).willReturn(d1);
+
+        Doctor updatedDoctor = doctorServiceImpl.updateDoctor(d1.getPesel(), d2);
+
+        //assertEquals(d1, updatedDoctor);
+        verify(doctorRepository, times(1)).save(d1);
+
+    }
+
+    @Test
+    public void shouldSaveDoctor() {
+        d1.setPesel(pesel);
+        given(doctorRepository.save(d1)).willReturn(d1);
+
+        Doctor doctorSaved = doctorServiceImpl.saveDoctor(d1);
+
+        assertEquals(d1, doctorSaved);
+    }
+
+    @Test
+    public void shouldDeleteDoctor() {
+        d1.setPesel(pesel);
+        lenient().when(doctorRepository.findById(d1.getPesel())).thenReturn(Optional.of(d1));
+
+        doctorServiceImpl.deleteDoctorByPesel(pesel);
+
+        verify(doctorRepository).deleteById(d1.getPesel());
     }
 
 }
