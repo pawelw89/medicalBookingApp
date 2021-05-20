c209374d9fe74481bd3e7c532747ee55d0d1a8a9
pawelw89 <pawelwojciechowski89@wp.pl>
2021-05-20 14:00:13 +0200

    Finished DoctorServiceImplTests Added PatientServiceImplTest

--- src/test/java/service/DoctorServiceImplTests.java
+++ src/test/java/service/DoctorServiceImplTests.java
@@ -1,29 +1,24 @@
-package com.pawelw89.Medical.Booking.App.service;
+package service;
 
 import entity.Doctor;
-import entity.Speciality;
-import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import repository.DoctorRepository;
-import service.DoctorServiceImpl;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class DoctorServiceImplTests {
+class DoctorServiceImplTests {
 
     @Mock
     private DoctorRepository doctorRepository;
@@ -32,66 +27,60 @@ public class DoctorServiceImplTests {
     private DoctorServiceImpl doctorServiceImpl;
 
     Doctor d1 = new Doctor();
-    long pesel = 12345678901l;
+    long pesel = 12345678901L;
     Doctor d2 = new Doctor();
     Doctor d3 = new Doctor();
 
-
     @Test
-    public void shouldReturnAllDoctors(){
-
+    void shouldReturnAllDoctors(){
         given(doctorRepository.findAll()).willReturn(Arrays.asList(d1, d2, d3));
 
         List<Doctor> result = doctorServiceImpl.getAllDoctors();
 
         assertThat(result).hasSize(3);
+        Assertions.assertEquals(List.of(d1, d2, d3), result);
     }
 
     @Test
-    public void shouldReturnDoctorByPesel(){
+    void shouldReturnDoctorByPesel(){
         d1.setPesel(pesel);
         given(doctorRepository.getOne(pesel)).willReturn(d1);
 
         Doctor foundDoctor = doctorServiceImpl.findDoctorByPesel(pesel);
 
-        assertEquals(d1, foundDoctor);
+        Assertions.assertEquals(d1, foundDoctor);
     }
 
-    //trzeba nad tym jeszcze popracować
     @Test
-    public void shouldUpdateDoctor(){
+    void shouldUpdateDoctorAndReturnUpdatedDoctorByPesel(){
         d1.setPesel(pesel);
-        d2.setFirstName("Jan");
-        d2.setSurname("Nowak");
         d2.setFirstName("Piotr");
-        d2.setSurname("Żyła");
-        given(doctorRepository.getOne(pesel)).willReturn(d1);
+        d2.setSurname("Nowak");
+        given(doctorRepository.getOne(pesel)).willReturn(d2);
 
         Doctor updatedDoctor = doctorServiceImpl.updateDoctor(d1.getPesel(), d2);
 
-        //assertEquals(d1, updatedDoctor);
-        verify(doctorRepository, times(1)).save(d1);
-
+        Assertions.assertEquals(d2, updatedDoctor);
+        Assertions.assertEquals("Piotr", updatedDoctor.getFirstName());
     }
 
     @Test
-    public void shouldSaveDoctor() {
+    void shouldSaveAndReturnNewDoctorByPesel() {
         d1.setPesel(pesel);
         given(doctorRepository.save(d1)).willReturn(d1);
 
         Doctor doctorSaved = doctorServiceImpl.saveDoctor(d1);
 
-        assertEquals(d1, doctorSaved);
+        Assertions.assertEquals(d1, doctorSaved);
     }
 
     @Test
-    public void shouldDeleteDoctor() {
+    void shouldDeleteDoctorByPesel() {
         d1.setPesel(pesel);
-        lenient().when(doctorRepository.findById(d1.getPesel())).thenReturn(Optional.of(d1));
 
         doctorServiceImpl.deleteDoctorByPesel(pesel);
 
-        verify(doctorRepository).deleteById(d1.getPesel());
+        verify(doctorRepository, times(1)).deleteById(pesel);
     }
 
 }
---
+++ src/test/java/service/PatientServiceImplTest.java
@@ -0,0 +1,85 @@
+package service;
+
+import entity.Patient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import repository.PatientRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PatientServiceImplTest {
+
+    @Mock
+    private PatientRepository patientRepository;
+
+    @InjectMocks
+    private PatientServiceImpl patientServiceImpl;
+
+    Patient p1 = new Patient();
+    long pesel = 12345678901L;
+    Patient p2 = new Patient();
+    Patient p3 = new Patient();
+
+    @Test
+    void shouldReturnAllPatients() {
+        given(patientRepository.findAll()).willReturn(Arrays.asList(p1, p2, p3));
+
+        List<Patient> result = patientServiceImpl.getAllPatients();
+
+        assertThat(result).hasSize(3);
+        Assertions.assertEquals(List.of(p1, p2, p3), result);
+    }
+
+    @Test
+    void shouldReturnPatientByPesel() {
+        p1.setPesel(pesel);
+        given(patientRepository.getOne(pesel)).willReturn(p1);
+
+        Patient foundPatient = patientServiceImpl.getPatientByPesel(pesel);
+
+        Assertions.assertEquals(p1, foundPatient);
+    }
+
+    @Test
+    void shouldUpdatePatientAndReturnUpdatedDoctorByPesel() {
+        p1.setPesel(pesel);
+        p2.setFirstName("Piotr");
+        p2.setSurname("Nowak");
+        given(patientRepository.getOne(pesel)).willReturn(p2);
+
+        Patient updatedPatient = patientServiceImpl.updatePatient(p1.getPesel(), p2);
+
+        Assertions.assertEquals(p2, updatedPatient);
+        Assertions.assertEquals("Piotr", updatedPatient.getFirstName());
+    }
+
+    @Test
+    void shouldSaveAndReturnNewPatientByPesel() {
+        p1.setPesel(pesel);
+        given(patientRepository.save(p1)).willReturn(p1);
+
+        Patient patientSaved = patientServiceImpl.savePatient(p1);
+
+        Assertions.assertEquals(p1, patientSaved);
+    }
+
+    @Test
+    void shouldDeletePatientByPesel() {
+        p1.setPesel(pesel);
+
+        patientServiceImpl.deletePatientByPesel(pesel);
+
+        verify(patientRepository, times(1)).deleteById(pesel);
+    }
+}
